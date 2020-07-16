9f467b7431444bf3363b59ce6f9997760330da49
cordis-dev <darius@adroiti.com>
2020-07-16 10:06:50 +0300

    Create t.cs

---
+++ t.cs
@@ -0,0 +1,12 @@
+public class T
+{
+   public static Logger Metrics(
+      this LoggerSink sink,
+      int days = -1)
+   {
+      const string logFileExtension = ".yml";
+   }
+   
+   private bool AllFieldsDone =>
+      new [] { one }.All(i => !string.IsNullOrWhiteSpace(i.text));
+}
